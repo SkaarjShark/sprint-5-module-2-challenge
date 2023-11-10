49bb60afa9552a7e5cd186d2c2abfb4743ecac7e
Jacob Handley <skaarjshark@outlook.com>
2023-11-10 14:33:22 -0600

    those DAMN mosquitoesgit remote add origin https://github.com/SkaarjShark/sprint-5-module-2-challenge.git

---
+++ module-project-events-in-the-browser-SkaarjShark-main/frontend/index.js
@@ -0,0 +1,126 @@
+// 👉 TASK 1 - Understand the existing code 👈
+function moduleProject2() {
+  // 👇 WORK WORK BELOW THIS LINE 👇
+  let startTime = new Date().getTime() // Record start time
+
+  function getTimeElapsed() { // To be used at end of game to get elapsed time
+    let currentTime = new Date().getTime()
+    return currentTime - startTime
+  }
+
+  // Setting up the footer content
+  let footer = document.querySelector('footer')
+  let currentYear = new Date().getFullYear()
+  footer.textContent = `© BLOOM INSTITUTE OF TECHNOLOGY ${currentYear}`
+
+  let keys = { // To easily check `event.key` on keyboard events
+    space: ' ',
+    up: 'ArrowUp',
+    right: 'ArrowRight',
+    down: 'ArrowDown',
+    left: 'ArrowLeft',
+  }
+
+  // Helper function to grab all squares
+  const getAllSquares = () => document.querySelectorAll('.square')
+
+  // Populating the grid with rows and squares
+  for (let n = 0; n < 5; n++) {
+    // Creating the rows
+    let row = document.createElement('div')
+    document.querySelector('#grid').appendChild(row)
+    row.classList.add('row')
+    // Creating the squares
+    for (let m = 0; m < 5; m++) {
+      let square = document.createElement('div')
+      square.classList.add('square')
+      row.appendChild(square)
+      square.addEventListener('click', () => {
+        // 👉 TASK 2 - Use a click handler to target a square 👈
+        allSquares.forEach(sq => {
+          if (sq.classList.contains("targeted")) sq.classList.remove("targeted")
+        })
+        square.classList.add("targeted")
+      })
+    }
+  }
+  document.querySelector('.row:nth-child(3)')
+    .children[2].classList.add('targeted') // Initial square being targeted
+
+  // Helper function to obtain 5 random indices (0-24) to put mosquitoes in
+  function generateRandomIntegers() {
+    let randomInts = []
+    while (randomInts.length < 5) {
+      let randomInt = Math.floor(Math.random() * 25)
+      if (!randomInts.includes(randomInt)) {
+        randomInts.push(randomInt)
+      }
+    }
+    return randomInts
+  }
+  let allSquares = getAllSquares()
+  generateRandomIntegers().forEach(randomInt => { // Puts live mosquitoes in 5 random squares
+    let mosquito = document.createElement('img')
+    mosquito.src = './mosquito.png'
+    mosquito.style.transform = `rotate(${Math.floor(Math.random() * 359)}deg) scale(${Math.random() * 0.4 + 0.8})`
+    mosquito.dataset.status = 'alive'
+    allSquares[randomInt].appendChild(mosquito)
+  })
+  
+  document.addEventListener('keydown', evt => {
+    // 👉 TASK 3 - Use the arrow keys to highlight a new square 👈
+    let targeted = document.querySelector(".targeted")
+    if (evt.key === 'ArrowUp') {
+      if (targeted.parentElement.previousElementSibling) {
+        idx = Array.from(targeted.parentElement.children).indexOf(targeted)
+        targeted.classList.remove("targeted")
+        targeted.parentElement.previousElementSibling.children[idx].classList.add('targeted')
+      }
+    }
+    if (evt.key === 'ArrowDown') {
+      if (targeted.parentElement.nextElementSibling) {
+        idx = Array.from(targeted.parentElement.children).indexOf(targeted)
+        targeted.classList.remove("targeted")
+        targeted.parentElement.nextElementSibling.children[idx].classList.add('targeted')
+      }
+    }
+    if (evt.key === 'ArrowLeft') {
+      if (targeted.previousElementSibling) {
+        targeted.classList.remove("targeted")
+        targeted.previousElementSibling.classList.add("targeted")
+      }
+    }
+    if (evt.key === 'ArrowRight') {
+      if (targeted.nextElementSibling) {
+        targeted.classList.remove("targeted")
+        targeted.nextElementSibling.classList.add("targeted")
+      }
+    }
+    // 👉 TASK 4 - Use the space bar to exterminate a mosquito 👈
+    if (evt.key === ' ') {
+      let mosquito = targeted.firstChild
+      if (mosquito && mosquito.dataset.status === 'alive') {
+        mosquito.dataset.status = "dead"
+        mosquito.parentElement.style.backgroundColor = "red"
+      }
+      // 👉 TASK 5 - End the game 👈
+      let liveMosquitoes = document.querySelectorAll('[data-status=alive]')
+      if (!liveMosquitoes.length) {
+        let elasped = getTimeElapsed() 
+        document.querySelector("p.info").textContent = `Extermination completed in ${elasped/1000} seconds!`
+        // Restart Button
+        let restartButton = document.createElement("Button")
+        restartButton.textContent = "Restart"
+        restartButton.addEventListener("click", () => location.reload())
+        document.querySelector("h2").insertAdjacentElement('beforeend', restartButton)
+      }
+    }
+  })
+  // 👆 WORK WORK ABOVE THIS LINE 👆
+}
+
+// ❗ DO NOT MODIFY THE CODE BELOW
+// ❗ DO NOT MODIFY THE CODE BELOW
+// ❗ DO NOT MODIFY THE CODE BELOW
+if (typeof module !== 'undefined' && module.exports) module.exports = { moduleProject2 }
+else moduleProject2()
